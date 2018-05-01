cd683f826a91e42d00dd6ab5a4bf2ded1e47cae1
Richard Smiley <rsmiley@gmail.com>
2018-04-30 23:39:14 -0400

    dsgdg

--- docs/hotdonuts.js
+++ docs/hotdonuts.js
@@ -14,6 +14,7 @@ hd.controller('DonutsController', ['$scope','$http','$location', function($scope
     } else if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(positionReceived);
     };
+    setTimeout(CallTrk.swap, 0);
   }
 
   function populateCurrent(data) {
@@ -24,7 +25,6 @@ hd.controller('DonutsController', ['$scope','$http','$location', function($scope
   function populateLocation(id) {
     return function(data) {
       $scope.locationHistory[id] = data.data;
-      setTimeout(CallTrk.swap, 0);
     }
   }
 
